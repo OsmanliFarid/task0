c92dd1748201be04c42bd724282cf3343590394f
OsmanliFarid <osmanliferid686@gmail.com>
2025-06-12 21:42:59 +0400

    task

---
+++ app.js
@@ -0,0 +1,41 @@
+const users = [
+  {
+    id: 1,
+    name: "Ramin Məmmədzadə",
+    age: 23,
+    email: "ramin@example.com",
+    role: "frontend developer",
+  },
+  {
+    id: 2,
+    name: "Yaqut Fəttah",
+    age: 21,
+    email: "yaqut@example.com",
+    role: "fullstack developer",
+  },
+  {
+    id: 3,
+    name: "Əli Əsədzadə",
+    age: 22,
+    email: "eli@example.com",
+    role: "backend developer",
+  },
+  {
+    id: 4,
+    name: "Fatimə Qurbanova",
+    age: 20,
+    email: "fatime@example.com",
+    role: "frontend developer",
+  },
+];
+
+//funksiya yazırsınız.Adı olsun findUser
+//bu funksiya arqument kimi array alir,ikinci arqument ad alir
+//arqument kimi aldigi arrayin icerisinde bu ad varsa hemin obyekti konsola qaytarsin
+
+const findUser = (user, username) => {
+  console.log(user);
+  const newuser = user.find((item) => item.name.includes(username));
+  console.log(newuser);
+};
+findUser(users, "Əli");
