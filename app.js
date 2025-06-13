12953beae6e43f6bf03f04d2a085c817302f47af
OsmanliFarid <osmanliferid686@gmail.com>
2025-06-13 19:41:46 +0400

    task

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
-const users = [
-  {
-    id: 1,
-    name: "Ramin Məmmədzadə",
-    age: 23,
-    email: "ramin@example.com",
-    role: "frontend developer",
-  },
-  {
-    id: 2,
-    name: "Yaqut Fəttah",
-    age: 21,
-    email: "yaqut@example.com",
-    role: "fullstack developer",
-  },
-  {
-    id: 3,
-    name: "Əli Əsədzadə",
-    age: 22,
-    email: "eli@example.com",
-    role: "backend developer",
-  },
-  {
-    id: 4,
-    name: "Fatimə Qurbanova",
-    age: 20,
-    email: "fatime@example.com",
-    role: "frontend developer",
-  },
-];
+import fs from "fs";
+let mesaj = "";
+fs.readFile("./task.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log("xeta bas verdi");
+  } else {
+    mesaj = data.toUpperCase();
 
-//funksiya yazırsınız.Adı olsun findUser
-//bu funksiya arqument kimi array alir,ikinci arqument ad alir
-//arqument kimi aldigi arrayin icerisinde bu ad varsa hemin obyekti konsola qaytarsin
+    fs.writeFile("./new.txt", mesaj, (err) => {
+      if (err) {
+        console.log("xeta bas verdi");
+      } else {
+        console.log("ugurlu");
+      }
+    });
+  }
+});
